bf13f4af5fe2d0368b4bc21792a5c7e79a7638cc
Arysill <125421389+Arysill@users.noreply.github.com>
2023-06-29 14:11:32 -0500

    n

--- js/task-04.js
+++ js/task-04.js
@@ -1,5 +1,5 @@
 const buttonMinus = document.querySelector('[data-action="decrement"]');
-const buttonPlus = document.querySelector('[data-action="incremet"]');
+const buttonPlus = document.querySelector('[data-action="increment"]');
 const valueUpdate = document.querySelector("#value")
 let counterValue = 0;
 buttonMinus.addEventListener('click', () => { 
